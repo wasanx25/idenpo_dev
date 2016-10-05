06bd42b6e9481dc7583a7e92935a5053b30c5b61
wataru0225 <wataru.kikuchi0225@gmail.com>
2016-10-06 00:01:18 +0900

    [modified]restore route before updating rails5

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  resources :posts, :except => [:new, :edit, :index]
+  root to: 'posts#index'
+  get '/get_post', to: 'posts#get_post'
 end
